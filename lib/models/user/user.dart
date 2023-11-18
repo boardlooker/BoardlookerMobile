dbb323e21a99446c7eb0532315cfe3fa0c5dea8b
Roman Galinovsky <galinovsky@kraud.ru>
2023-11-19 00:24:15 +0300

    add: user dto

---
+++ lib/models/user/user.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+const factory User({
+  required final String username,
+  required final String password,
+  required final String fullName,
+  required final String birthdate
+}) = _User;
+
+factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
