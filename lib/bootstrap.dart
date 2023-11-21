02e7f24a5c9669ca0565b160097f395f95104590
Roman Galinovsky <galinovsky@kraud.ru>
2023-11-21 17:52:59 +0300

    add: portait orientation pinned

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Bootstrap extends StatelessWidget {
   const Bootstrap({super.key});
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Boardlooker',
       theme: ThemeData(
