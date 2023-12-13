17c0f8385a32f127e517bdf8775af69dd1b38781
Roman Galinovsky <galinovsky@kraud.ru>
2023-12-13 03:25:13 +0300

    add: theme

--- lib/shared/index.dart
+++ lib/shared/index.dart
@@ -2,3 +2,4 @@ export 'secure_storage.dart';
 export 'constants.dart';
 export 'enums/index.dart';
 export 'bloc_observer.dart';
+export 'theme.dart';
---
+++ lib/shared/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+    seedColor: Colors.deepPurple,
+  background: Colors.white,
+  surface: const Color(0xFFD9EFB3)
+);
