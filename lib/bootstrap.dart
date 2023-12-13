d0433d1a17a4e666e02e54196db95482c800040d
Roman Galinovsky <galinovsky@kraud.ru>
2023-12-13 04:10:12 +0300

    add: root router

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,12 @@
-import 'package:boardlooker_mobile/screens/index.dart';
+import 'package:boardlooker_mobile/blocs/index.dart';
+import 'package:boardlooker_mobile/repositories/index.dart';
+import 'package:boardlooker_mobile/router/index.dart';
 import 'package:boardlooker_mobile/shared/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get_it/get_it.dart';
 
 class Bootstrap extends StatelessWidget {
   const Bootstrap({super.key});
@@ -12,13 +17,47 @@ class Bootstrap extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return MaterialApp(
-      title: 'Boardlooker',
-      theme: ThemeData(
-        colorScheme: colorScheme,
-        useMaterial3: true,
-      ),
-      home: const RegistrationScreen(),
-    );
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(
+              create: (BuildContext context) =>
+                  AuthBloc(
+                      GetIt.instance.get<IAuthRepository>(),
+                      GetIt.instance.get<SecureStorage>()
+                  )
+          ),
+          BlocProvider(
+            create: (BuildContext context) => InternetAccessCubit(),
+          ),
+          BlocProvider(
+            create: (BuildContext context) => FilesystemBloc(),
+          ),
+        ],
+        child: BlocListener<InternetAccessCubit, InternetAccessState>(
+          listener: (context, state) {},
+          child: BlocBuilder<AuthBloc, AuthState>(
+            buildWhen: (previous, current) => previous.authStatus != current.authStatus,
+            builder: (context, state) {
+              return MaterialApp.router(
+                localizationsDelegates: const [
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate
+                ],
+                supportedLocales: const [
+                  Locale('en', 'US'),
+                  Locale('ru', 'RU'),
+                ],
+                routerConfig: authRouter(),
+                theme: ThemeData(
+                  colorScheme: colorScheme,
+                  useMaterial3: true,
+                ),
+                title: 'Boardlooker',
+              );
+            },
+
+        )
+    ));
   }
 }
