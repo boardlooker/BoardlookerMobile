cf79c2aee7ec214c0e3e3c092fc9c29b81a315eb
Roman Galinovsky <galinovsky@kraud.ru>
2023-12-14 22:04:27 +0300

    add: floor db

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'dart:developer';
 import 'dart:io';
 
 import 'package:boardlooker_mobile/bootstrap.dart';
+import 'package:boardlooker_mobile/db/db_client.dart';
 import 'package:boardlooker_mobile/di/index.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -13,6 +15,7 @@ void main() async {
   await runZonedGuarded(() async {
     await dotenv.load(fileName: ".env", mergeWith: Platform.environment);
     configureDependencies();
+    await GetIt.instance.get<DbClient>().initDb();
     // Bloc.observer = BlocGlobalObserver();
     HydratedBloc.storage = await HydratedStorage.build(
         storageDirectory: await getApplicationDocumentsDirectory());
