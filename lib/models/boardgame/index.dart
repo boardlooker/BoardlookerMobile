875d0c66294c77d467a0583d248d282ad6ed0518
Roman Galinovsky <galinovsky@kraud.ru>
2023-12-18 02:23:02 +0300

    add: indexes

--- lib/models/boardgame/index.dart
+++ lib/models/boardgame/index.dart
@@ -1,4 +1,3 @@
 export 'boardgame.dart';
 export 'boardgame_floor.dart';
 export 'mapper.dart';
-export 'boardgame_dao.dart';
--- lib/models/location/index.dart
+++ lib/models/location/index.dart
@@ -1,3 +1,2 @@
 export 'location.dart';
 export 'location_floor.dart';
-export 'location_dao.dart';
